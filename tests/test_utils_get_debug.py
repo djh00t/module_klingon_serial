51154ab68ca90e5d80a4dd66df1f875786d1f427
David Hooton <david@hooton.org>
2023-10-06 12:26:36 +1100

    aider: Added /Users/djh/work/src/github.com/djh00t/module_klingon/Makefile

--- tests/test_utils_get_debug.py
+++ tests/test_utils_get_debug.py
@@ -1,127 +1,45 @@
 import os
-import netifaces2 as netifaces  # type: ignore
 import pytest
+import netifaces
 import uuid
 from strtobool import strtobool  # type: ignore
 
 
-@pytest.fixture
-def setup_env():
-    os.environ["DEBUG"] = "True"
+def test_get_debug_env_true():
+    os.environ["DEBUG"] = "true"
+    assert get_debug() == True
 
 
-def test_get_debug_returns_true_when_environment_variable_is_set_to_true(setup_env):
-    assert get_debug() is True
+def test_get_debug_env_false():
+    os.environ["DEBUG"] = "false"
+    assert get_debug() == False
 
 
-def test_get_debug_returns_false_when_environment_variable_is_not_set():
-    assert get_debug() is False
+def test_get_debug_env_none():
+    os.environ["DEBUG"] = None
+    assert get_debug() == False
 
 
-def test_get_debug_returns_false_when_environment_variable_is_set_to_false():
-    os.environ["DEBUG"] = "False"
-    assert get_debug() is False
+def test_get_debug_invalid_input():
+    os.environ["DEBUG"] = "Invalid"
+    assert get_debug() == False
 
 
-def test_get_debug_returns_false_when_environment_variable_is_not_boolean():
-    os.environ["DEBUG"] = "invalid"
-    assert get_debug() is False
-
-
-def test_get_mac_address_and_interface_returns_correct_tuple_when_mac_address_is_found(
-    monkeypatch,
-):
-    mock_mac_address = "00:11:22:33:44:55"
+def test_get_mac_address_and_interface_valid():
     mock_interface = "eth0"
-
-    def mock_interfaces():
-        return [mock_interface]
-
-    def mock_ifaddresses(interface):
-        return {netifaces.AF_LINK: [{"addr": mock_mac_address}]}
-
-    monkeypatch.setattr(netifaces, "interfaces", mock_interfaces)
-    monkeypatch.setattr(netifaces, "ifaddresses", mock_ifaddresses)
+    mock_mac_address = "00:11:22:33:44:55"
+    mock_mac_address_int = int(mock_mac_address.replace(":", ""), 16)
     assert get_mac_address_and_interface() == (mock_mac_address, mock_interface)
 
 
-def test_get_mac_address_and_interface_returns_none_tuple_when_mac_address_is_not_found(
-    monkeypatch,
-):
-    def mock_interfaces():
-        return []
-
-    monkeypatch.setattr(netifaces, "interfaces", mock_interfaces)
+def test_get_mac_address_and_interface_invalid_interface():
+    mock_interface = "invalid"
+    mock_mac_address = "00:11:22:33:44:55"
+    mock_mac_address_int = int(mock_mac_address.replace(":", ""), 16)
     assert get_mac_address_and_interface() == (None, None)
 
 
-def test_get_mac_address_and_interface_returns_none_tuple_when_mac_address_is_found_but_not_matching(
-    monkeypatch,
-):
-    mock_mac_address = "00:11:22:33:44:55"
+def test_get_mac_address_and_interface_invalid_mac_address():
     mock_interface = "eth0"
-
-    def mock_interfaces():
-        return [mock_interface]
-
-    def mock_ifaddresses(interface):
-        return {netifaces.AF_LINK: [{"addr": "FF:EE:DD:CC:BB:AA"}]}
-
-    monkeypatch.setattr(netifaces, "interfaces", mock_interfaces)
-    monkeypatch.setattr(netifaces, "ifaddresses", mock_ifaddresses)
-    assert get_mac_address_and_interface() == (None, None)
-
-
-def test_get_mac_address_and_interface_returns_none_tuple_when_mac_address_cannot_be_converted_to_int(
-    monkeypatch,
-):
-    mock_mac_address = "00:11:22:33:44:55"
-    mock_interface = "eth0"
-
-    def mock_interfaces():
-        return [mock_interface]
-
-    def mock_ifaddresses(interface):
-        return {netifaces.AF_LINK: [{"addr": mock_mac_address}]}
-
-    def mock_getnode():
-        raise ValueError
-
-    monkeypatch.setattr(netifaces, "interfaces", mock_interfaces)
-    monkeypatch.setattr(netifaces, "ifaddresses", mock_ifaddresses)
-    monkeypatch.setattr(uuid, "getnode", mock_getnode)
-    assert get_mac_address_and_interface() == (None, None)
-
-
-def test_get_mac_address_and_interface_returns_none_tuple_when_key_error_is_raised(
-    monkeypatch,
-):
-    mock_mac_address = "00:11:22:33:44:55"
-    mock_interface = "eth0"
-
-    def mock_interfaces():
-        return [mock_interface]
-
-    def mock_ifaddresses(interface):
-        raise KeyError
-
-    monkeypatch.setattr(netifaces, "interfaces", mock_interfaces)
-    monkeypatch.setattr(netifaces, "ifaddresses", mock_ifaddresses)
-    assert get_mac_address_and_interface() == (None, None)
-
-
-def test_get_mac_address_and_interface_returns_none_tuple_when_index_error_is_raised(
-    monkeypatch,
-):
-    mock_mac_address = "00:11:22:33:44:55"
-    mock_interface = "eth0"
-
-    def mock_interfaces():
-        return [mock_interface]
-
-    def mock_ifaddresses(interface):
-        return {netifaces.AF_LINK: [{}]}
-
-    monkeypatch.setattr(netifaces, "interfaces", mock_interfaces)
-    monkeypatch.setattr(netifaces, "ifaddresses", mock_ifaddresses)
+    mock_mac_address = "Invalid"
     assert get_mac_address_and_interface() == (None, None)
