257cfcd9268047504058ab39aa5804ba19ca29a3
David Hooton <david@hooton.org>
2023-10-06 12:32:01 +1100

    Refactored tests for `get_debug` and `get_mac_address_and_interface` functions.

--- tests/test_utils_get_debug.py
+++ tests/test_utils_get_debug.py
@@ -1,46 +1,75 @@
+from klingon import utils
 import os
 import pytest
 import netifaces
 import uuid
 from strtobool import strtobool  # type: ignore
-from klingon.utils import get_debug, get_mac_address_and_interface
 
 
-def test_get_debug_env_true():
-    os.environ["DEBUG"] = "true"
+def test_get_debug_enabled():
+    os.environ["DEBUG"] = "1"
     assert get_debug() == True
 
 
-def test_get_debug_env_false():
-    os.environ["DEBUG"] = "false"
+def test_get_debug_disabled():
+    os.environ["DEBUG"] = "0"
     assert get_debug() == False
 
 
-def test_get_debug_env_none():
-    os.environ["DEBUG"] = None
+def test_get_debug_invalid():
+    os.environ["DEBUG"] = "test"
     assert get_debug() == False
 
 
-def test_get_debug_invalid_input():
-    os.environ["DEBUG"] = "Invalid"
+def test_get_debug_not_set():
+    if "DEBUG" in os.environ:
+        del os.environ["DEBUG"]
     assert get_debug() == False
 
 
-def test_get_mac_address_and_interface_valid():
-    mock_interface = "eth0"
-    mock_mac_address = "00:11:22:33:44:55"
-    mock_mac_address_int = int(mock_mac_address.replace(":", ""), 16)
-    assert get_mac_address_and_interface() == (mock_mac_address, mock_interface)
+def test_get_mac_address_and_interface_found():
+    mock_netifaces = {
+        "interfaces": ["lo", "eth0"],
+        "ifaddresses": {
+            "lo": {netifaces.AF_LINK: [{"addr": "00:00:00:00:00:00"}]},
+            "eth0": {netifaces.AF_LINK: [{"addr": "00:11:22:33:44:55"}]},
+        },
+    }
+    # monkey patch netifaces to return the mocked data
+    original_netifaces = netifaces
+    netifaces = type("MockNetifaces", (object,), mock_netifaces)
+    assert get_mac_address_and_interface() == ("00:11:22:33:44:55", "eth0")
+    # revert monkey patch
+    netifaces = original_netifaces
 
 
-def test_get_mac_address_and_interface_invalid_interface():
-    mock_interface = "invalid"
-    mock_mac_address = "00:11:22:33:44:55"
-    mock_mac_address_int = int(mock_mac_address.replace(":", ""), 16)
+def test_get_mac_address_and_interface_not_found():
+    mock_netifaces = {
+        "interfaces": ["lo", "eth0"],
+        "ifaddresses": {
+            "lo": {netifaces.AF_LINK: [{"addr": "00:00:00:00:00:00"}]},
+            "eth0": {netifaces.AF_LINK: [{"addr": "11:22:33:44:55:66"}]},
+        },
+    }
+    # monkey patch netifaces to return the mocked data
+    original_netifaces = netifaces
+    netifaces = type("MockNetifaces", (object,), mock_netifaces)
     assert get_mac_address_and_interface() == (None, None)
+    # revert monkey patch
+    netifaces = original_netifaces
 
 
-def test_get_mac_address_and_interface_invalid_mac_address():
-    mock_interface = "eth0"
-    mock_mac_address = "Invalid"
+def test_get_mac_address_and_interface_invalid():
+    mock_netifaces = {
+        "interfaces": ["lo", "eth0"],
+        "ifaddresses": {
+            "lo": {netifaces.AF_LINK: [{"addr": "test"}]},
+            "eth0": {netifaces.AF_LINK: [{"addr": "11:22:33:44:55:66"}]},
+        },
+    }
+    # monkey patch netifaces to return the mocked data
+    original_netifaces = netifaces
+    netifaces = type("MockNetifaces", (object,), mock_netifaces)
     assert get_mac_address_and_interface() == (None, None)
+    # revert monkey patch
+    netifaces = original_netifaces
