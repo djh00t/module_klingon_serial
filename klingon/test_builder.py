7fd1004742b06d4e64a4dca01999a2c620897792
David Hooton <david@hooton.org>
2023-10-06 12:52:40 +1100

    aider: Modify prompt to ensure all comments are prefixed with "# " in `generate_tests` function.
    # Aider chat conversation:
    
    USER: Some files are sneaking through without valid python and comments.
    
    ASSISTANT: To ensure that the generated test files contain valid Python code and comments, we can add a validation step in the `generate_tests` function
    ^C KeyboardInterrupt
    
    USER: Some files are sneaking through without valid python, instead they contain uncommented comments. Adjust the prompt to ensure that openai only returns with valid code and that all comments are prefixed with "# "
    
    ASSISTANT: To ensure that the generated test files contain valid Python code and that all comments are prefixed with "# ", we can modify the prompt that we send to the OpenAI API in the `generate_tests` function in `klingon/test_builder.py`.
    
    Here is the *edit block* for the changes:
    
    <source>python
    klingon/test_builder.py
    <<<<<<< HEAD
                        prompt = f"Generate pytest tests that cover 100% of the following function:\n\n```python\n{function_code}\n```\n\nEnsure that the tests cover all edge cases and provide meaningful assertions. Only return the testing code as a snippet. Do not include any comments."
    =======
                        prompt = f"Generate pytest tests that cover 100% of the following function:\n\n```python\n{function_code}\n```\n\nEnsure that the tests cover all edge cases and provide meaningful assertions. Only return the testing code as a snippet. All comments should be prefixed with '# '."
    >>>>>>> updated
    </source>
    
    This change will instruct the OpenAI API to prefix all comments with "# ", ensuring that they are valid Python comments.

--- klingon/test_builder.py
+++ klingon/test_builder.py
@@ -94,7 +94,7 @@ def generate_tests(functions):
                     tree = ast.parse(code)
                     function_node = next(node for node in ast.walk(tree) if isinstance(node, ast.FunctionDef) and node.name == function)
                     function_code = ast.unparse(function_node)
-                    prompt = f"Generate pytest tests that cover 100% of the following function:\n\n```python\n{function_code}\n```\n\nEnsure that the tests cover all edge cases and provide meaningful assertions. Only return the testing code as a snippet. Do not include any comments."
+                    prompt = f"Generate pytest tests that cover 100% of the following function:\n\n```python\n{function_code}\n```\n\nEnsure that the tests cover all edge cases and provide meaningful assertions. Only return the testing code as a snippet. All comments should be prefixed with '# '."
                     prompt_chunks = chunk_prompt(prompt)
                     responses = []
                     for chunk, length in prompt_chunks:
