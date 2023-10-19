c2956c1e7ce4f3a6c188037cd8a1c9eb3a495dd6
David Hooton <david@hooton.org>
2023-10-19 17:12:54 +1100

    Change to psutil instead of netifaces

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'datetime',
-        'netifaces',
+        'psutil',
         'pytest',
         'uuid',
         'setuptools'
