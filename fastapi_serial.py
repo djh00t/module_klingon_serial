1a5dff011ddd2112d04db449e53ad5fcc8bf2707
David Hooton <david@hooton.org>
2023-10-19 11:25:06 +1100

    aider: Created `fastapi_serial.py` with two routes: GET `/` returns a JSON object with a generated serial in uppercase, and GET `/test` runs pytest tests and returns the results as a JSON object. The script auto-reloads when code is changed and runs as a daemon.
    # Aider chat conversation:
    
    USER: Create fastapi_serial.py which when run from commandline launches a fastapi server with two routes GET / which returns a json object with "serial" as the key and a serial generated by klingon_serial in upper case as the value. The second route is /test which runs the pytest tests in ./tests/ and returns the results as a json object. fastapi_serial.py should auto-reload when code is changed and should run as a daemon when run using "python fastapi_serial.py" without having to call uvicorn directly
    
    ASSISTANT: To create `fastapi_serial.py` with the specified requirements, we need to create a new file `fastapi_serial.py` and add the necessary code to it.
    
    Here is the code:
    
    ```python
    fastapi_serial.py
    <<<<<<< HEAD
    
    =======
    import os
    import subprocess
    import json
    from fastapi import FastAPI
    from klingon_serial.generate_serial import generate_serial
    
    app = FastAPI()
    
    @app.get("/")
    def read_root():
        serial = generate_serial()
        return {"serial": serial.upper()}
    
    @app.get("/test")
    def run_tests():
        result = subprocess.run(["pytest", "./tests/"], capture_output=True, text=True)
        return {"result": result.stdout}
    
    if __name__ == "__main__":
        os.system("uvicorn fastapi_serial:app --reload --daemon")
    >>>>>>> updated
    ```
    
    This code creates a FastAPI application with two routes. The first route, `/`, generates a serial using the `klingon_serial` module and returns it in uppercase. The second route, `/test`, runs the pytest tests in the `./tests/` directory and returns the results.
    
    The script is set to auto-reload when code is changed and runs as a daemon when run using `python fastapi_serial.py`. This is achieved by running the uvicorn server with the `--reload` and `--daemon` options in the main function.

---
+++ fastapi_serial.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+import json
+from fastapi import FastAPI
+from klingon_serial.generate_serial import generate_serial
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    serial = generate_serial()
+    return {"serial": serial.upper()}
+
+@app.get("/test")
+def run_tests():
+    result = subprocess.run(["pytest", "./tests/"], capture_output=True, text=True)
+    return {"result": result.stdout}
+
+if __name__ == "__main__":
+    os.system("uvicorn fastapi_serial:app --reload --daemon")
