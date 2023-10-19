9ee7c32786768190b050e77f4f6fcd8eee65cede
David Hooton <david@hooton.org>
2023-10-19 11:21:00 +1100

    aider: Make `serial.py` return the serial in all caps with no other content.
    # Aider chat conversation:
    
    USER: Make serial.
    
    ASSISTANT:
    ^C KeyboardInterrupt
    
    USER: Make `serial.py` return the serial in all caps with no other content.
    
    ASSISTANT: To make `serial.py` return the serial in all caps with no other content, we need to modify the `serial.py` file.
    
    Here are the changes:
    
    ```python
    serial.py
    <<<<<<< HEAD
    from klingon_serial.generate_serial import generate_serial
    
    # Generate a unique serial number
    unique_serial = generate_serial()
    print(f"Generated Serial: {unique_serial}")
    =======
    from klingon_serial.generate_serial import generate_serial
    
    # Generate a unique serial number
    unique_serial = generate_serial()
    print(unique_serial.upper())
    >>>>>>> updated
    ```
    
    This change will make the `serial.py` script print the generated serial number in all uppercase letters.

--- serial.py
+++ serial.py
@@ -2,4 +2,4 @@ from klingon_serial.generate_serial import generate_serial
 
 # Generate a unique serial number
 unique_serial = generate_serial()
-print(f"Generated Serial: {unique_serial}")
+print(unique_serial.upper())
