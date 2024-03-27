e326f2c1febc14e29223037f8243e90288b72047
David Hooton <david@hooton.org>
2024-03-27 11:09:26 +1100

    Added openfaas/handler.py file.

---
+++ openfaas/handler.py
@@ -0,0 +1,9 @@
+from klingon_serial import generate_serial
+
+def main(context=None, event=None):
+    # Generate a unique serial number
+    unique_serial = generate_serial()
+    return {"status": 200, "message": "OK", "serial": unique_serial.upper()}
+
+if __name__ == "__main__":
+    main(None, None)
