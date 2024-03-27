93c92846fe7bb61608f88b008378280d5dd24b25
David Hooton <david@hooton.org>
2024-03-27 13:50:51 +1100

    Add support for returning HTML response based on accept header.

--- openfaas/wrapper.py
+++ openfaas/wrapper.py
@@ -25,6 +25,9 @@ async def root(accept: Optional[str] = Header(None)):
     if accept:
         if "text/plain" in accept:
             return PlainTextResponse(unique_serial)
+        elif "text/html" in accept:
+            html_content = f'<html><body><p>{unique_serial}</p></body></html>'
+            return HTMLResponse(content=html_content)
         elif "application/xml" in accept or "text/xml" in accept or "application/xhtml+xml" in accept:
             xml_content = f'<root><serial>{unique_serial}</serial></root>'
             return XMLResponse(content=xml_content)
