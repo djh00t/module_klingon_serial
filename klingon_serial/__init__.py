43573cee78203f329955d88cd350e3cb15ebe96c
David Hooton <david@hooton.org>
2024-10-10 13:36:37 +1100

    feat(__init__.py): add main function with serial generation and logging
    Added a main function to generate and validate a serial number when the 
    module is executed as a script. Integrated logging for tracking serial 
    generation and validation results.
    
    Signed-off-by: David Hooton <david@hooton.org>

--- klingon_serial/__init__.py
+++ klingon_serial/__init__.py
@@ -1,3 +1,23 @@
-from .strtobool import strtobool
-from .utils import get_mac_address_and_interface, get_debug, validate_serial
-from .generate import get_mac_address_hex,get_process_id, get_millisecond_epoch_hex, generate_serial
+import logging
+
+from .utils import validate_serial
+from .generate import (
+    generate_serial,
+)
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def main():
+    """Main function to run when the module is executed as a script."""
+    serial = generate_serial()
+    print(serial)
+    logging.debug(f"Generated Serial: {serial}")
+    logging.debug(f"Serial Valid: {validate_serial(serial)}")
+
+
+if __name__ == "__main__":
+    main()
